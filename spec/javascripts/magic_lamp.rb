2364cd04362d0db5c99842ea965ccce889b40d49
end3rs-army <eric.m.dowty@gmail.com>
2015-07-08 10:40:41 -0600

    created database resources in the MagicLamp file

--- spec/javascripts/magic_lamp.rb
+++ spec/javascripts/magic_lamp.rb
@@ -1,3 +1,9 @@
 MagicLamp.fixture do 
+
+  Idea.create([
+    { title: "First idea", body: "this is the first body" },
+    { title: "Second idea", body: "this is the second body" }
+  ])
+
   render template: 'ideas/index'
 end
